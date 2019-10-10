e47f49bd7ded6e9ddea0da869472bf8f83a91e4d
Kate Malone <katerosemalone@gmail.com>
2019-10-10 16:41:16 -0600

    Fixes loading page and Adds GiF

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -34,47 +34,42 @@ componentDidMount() {
 
   render() {
     return (
-      <form className="user_form">
-        <div className="user_name-div">
-          <label className="user_name-label">Your Name?</label>
-          <input className="user_name-input" type="text" value={this.state.name} name="name" onChange={this.handleChange} />
-        </div>
-        <div className="user_quote-div">
-          <label className="user_quote-label">Favorite SW Quote?</label>
-          <textarea className="user_quote-textarea" value={this.state.quote} name="quote" onChange={this.handleChange} />
-        </div>
-        {/* <label>How much do you love Star Wars?</label>
-        <select value={this.state.knowledgeLevel} onClick={this.handleChange}>
-          <option value="Star What?">Star What?</option>
-          <option value="Jedi in Training?">Jedi in Training</option>
-          <option value="My firtsborns name is yoda"> My firstborns name is Yoda</option>
-        </select>  */}
-        <div className="user_radio-div">
-          <label className="user_radio-label"> 
-            <input className="user_radio-input" type='radio' name="knowledgeLevel" value='Star What?' 
-            onChange={this.handleChange} /> 
-            Star What?
-          </label>
-        </div>
-        <div className="user_radio-div">
-          <label className="user_radio-label"> 
-            <input className="user_radio-input" type='radio' name="knowledgeLevel" value='Jedi in Training?' 
-            
-            onChange={this.handleChange} /> 
-            Jedi in Training
-          </label>
-        </div>
-        <div className="user_radio-div">
-          <label className="user_radio-label"> 
-            <input className="user_radio-input" type='radio' name="knowledgeLevel" value='My firtsborns name is yoda' 
-            onChange={this.handleChange} /> 
-            My Firstborns names is Yoda
-          </label>
-        </div> 
-        <button className="user-submit-btn" type="submit" onClick={this.handleFormSubmit}>SUBMIT</button>
-
-
-      </form>
+      <div className="user_profile-div">
+        <img className="user_profile-image" alt="Yo Jedi laser gif animation gif r2d2 cat menu starwars force jedi" src="https://cdn.dribbble.com/users/1539273/screenshots/6122439/spoilers_drib.gif" />
+        <form className="user_form">
+          <div className="user_name-div">
+            <label className="user_name-label">Your Name?</label>
+            <input className="user_name-input" type="text" value={this.state.name} name="name" onChange={this.handleChange} />
+          </div>
+          <div className="user_quote-div">
+            <label className="user_quote-label">Favorite SW Quote?</label>
+            <textarea className="user_quote-textarea" value={this.state.quote} name="quote" onChange={this.handleChange} />
+          </div>
+          <div className="user_radio-div">
+            <label className="user_radio-label"> 
+              <input className="user_radio-input" type='radio' name="knowledgeLevel" value='Star What?' 
+              onChange={this.handleChange} /> 
+              Star What?
+            </label>
+          </div>
+          <div className="user_radio-div">
+            <label className="user_radio-label"> 
+              <input className="user_radio-input" type='radio' name="knowledgeLevel" value='Jedi in Training?' 
+              
+              onChange={this.handleChange} /> 
+              Jedi in Training
+            </label>
+          </div>
+          <div className="user_radio-div">
+            <label className="user_radio-label"> 
+              <input className="user_radio-input" type='radio' name="knowledgeLevel" value='My firtsborns name is yoda' 
+              onChange={this.handleChange} /> 
+              My Firstborns names is Yoda
+            </label>
+          </div> 
+          <button className="user-submit-btn" type="submit" onClick={this.handleFormSubmit}>SUBMIT</button>
+        </form>
+      </div>
       )
   }
 }
