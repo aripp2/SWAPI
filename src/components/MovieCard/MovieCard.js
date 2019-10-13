05b2eaec935f9d9e50c3de27731a753292f7ee9e
Amy Rippeto <amyrippeto@gmail.com>
2019-10-13 14:48:54 -0600

    Convert episode numver to roman numerals

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,10 +5,16 @@ import { Link } from 'react-router-dom';
 const MovieCard = ({ id, title, date, characters, getCharacters }) => {
   const splitDate = date.split('-')
   let fixedDate = `${splitDate[1]}/${splitDate[2]}/${splitDate[0]}`
+  const convert = (num) => {
+    if(num < 1) {return ''}
+    if (num >= 4){return 'IV' + convert(num - 4)}
+    if (num >= 1){return 'I' + convert(num - 1)}
+  }
+  const roman = convert(id)
   return (
       <div className="movie_card">
         <div className="movie_card-style">
-          <h3 className="episode" >Episode {id}</h3>
+          <h3 className="episode" >Episode {roman}</h3>
           <h4 className="movie_card-title">{title}</h4>
           <h5>Released on {fixedDate}</h5>
         <Link to={`/movies/${id}`}><button className="movie_card-btn"
