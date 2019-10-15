f0e45d3722af45f0c3072109d218efdbdc1aa205
Kate Malone <katerosemalone@gmail.com>
2019-10-15 16:57:44 -0600

    Updates

---
+++ precache-manifest.a961c9fa1821fb8f889700b5bb382513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f15159b257659cf9f97fa12079d456",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95e08f68032e016a8f7b",
+    "url": "./static/css/main.3a6b9f20.chunk.css"
+  },
+  {
+    "revision": "f4ec5043adf5a5a5a05a",
+    "url": "./static/js/2.826c9cb3.chunk.js"
+  },
+  {
+    "revision": "95e08f68032e016a8f7b",
+    "url": "./static/js/main.c7b9acfb.chunk.js"
+  },
+  {
+    "revision": "974a963073fd9a504032",
+    "url": "./static/js/runtime-main.84e2fbcc.js"
+  }
+]);
